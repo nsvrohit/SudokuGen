5f6fb8f43a2e633a17479b31c31c2d1bc85c50af
V Wood <vwood.org@gmail.com>
2012-03-01 10:33:36 +1300

    Fix tiny bugs in BacktrackArray.py and add Tests

--- BacktrackArray.py
+++ BacktrackArray.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python
+
 class BacktrackArray():
 	"""A Bytearray that supports backtracking."""
 	def __init__(self, initial = ''):
 		self.stack = []
 		self.storage = bytearray(initial)
-		self.default = default
 
 	def set(self, index, value):
 		initial = self.storage[index]
@@ -12,8 +13,21 @@ class BacktrackArray():
 
 	def undo(self):
 		index, initial, value = self.stack.pop()
-		assert self.storage[index] = value
+		assert chr(self.storage[index]) == value
 		self.storage[index] = initial
 
 	def __str__(self):
 		return str(self.storage)
+
+if __name__ == '__main__':
+	print "Testing Backtrack Array..."
+	init = "initial contents"
+	ba = BacktrackArray(init)
+	assert init == str(ba)
+	ba.set(0, "z")
+	changed = 'z' + init[1:]
+	assert changed == str(ba)
+	ba.undo()
+	assert init == str(ba)
+	print "Tests passed."
+
